ded7c06fa749d27cc43c719bf87255d3d9cefc57
Guy Dupont <dupontgu@gmail.com>
2026-05-29 09:32:00 -0400

    add github packages for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import kotlin.apply
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -12,5 +15,24 @@ plugins {
 
 allprojects {
     group = "com.thelightphone.filemanager"
-    version = "0.0.1"
+    version = "0.0.2"
+
+    val localProperties = Properties().apply {
+        rootProject.file("local.properties").takeIf { it.exists() }?.inputStream()?.use { load(it) }
+    }
+
+    plugins.withId("maven-publish") {
+        extensions.configure<PublishingExtension> {
+            repositories {
+                maven {
+                    name = "GitHubPackages"
+                    url = uri("https://maven.pkg.github.com/lightphone/light-file-manager")
+                    credentials {
+                        username = localProperties.getProperty("gpr.user") ?: System.getenv("GITHUB_ACTOR")
+                        password = localProperties.getProperty("gpr.key") ?: System.getenv("GITHUB_TOKEN")
+                    }
+                }
+            }
+        }
+    }
 }
