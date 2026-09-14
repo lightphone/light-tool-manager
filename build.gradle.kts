e2ad91c1e8c0ff1d33d4ffcb0111ae7573a808c7
Guy Dupont <dupontgu@gmail.com>
2026-09-14 16:12:28 -0400

    show download picker when job has result file

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 
 allprojects {
     group = "com.thelightphone.toolmanager"
-    version = "0.0.12"
+    version = "0.0.13"
 
     plugins.withId("com.vanniktech.maven.publish") {
         extensions.configure<MavenPublishBaseExtension> {
